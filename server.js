7c5dabf9276bb423523c153a77a7b2062264c606
shivakrishnasuram <suramshivakrishnasuram@gmail.com>
2025-05-28 16:21:44 +0530

    initials

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get("/get", (req, res) => {
 });
 
 
-// connectDB();
+connectDB();
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
