fe7a7edb2bda4cf521878677978d7e8a6c4da087
shivakrishnasuram <suramshivakrishnasuram@gmail.com>
2025-05-28 15:07:36 +0530

    userdone

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+// const connectDB = require('./config/db');
+// const cors =require("cors")
+// const userRoutes = require('./routes/userRoutes');
+// const cors = require('cors')
+const app = express();
+// app.use(cors())
+
+// app.use(express.cors())
+app.use(express.json());
+// app.use('/', userRoutes);
+
+// connectDB();
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
